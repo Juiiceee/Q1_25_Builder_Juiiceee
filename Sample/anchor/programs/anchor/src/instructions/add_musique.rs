d6973924a8089c10b93eec0e8bf2ec46223ee634
Juiiceee <lbehr@student.42.fr>
2025-02-27 19:23:30 +0100

    ca fonctionne on peut add

--- Sample/anchor/programs/anchor/src/instructions/add_musique.rs
+++ Sample/anchor/programs/anchor/src/instructions/add_musique.rs
@@ -3,12 +3,13 @@ use anchor_lang::prelude::*;
 use crate::states::{Music, Musician, Style};
 
 #[derive(Accounts)]
-#[instruction(name: String, pubkey_artist: Pubkey)]
+#[instruction(name: String)]
 pub struct AddMusic<'info> {
-    #[account(init, payer = signer, seeds = [b"music", name.as_bytes(), pubkey_artist.key().as_ref()], bump, space = 8 + 50 + 11 + 8 + 200 + 200 + 4)]
+    #[account(init, payer = signer, seeds = [b"music", name.as_bytes(), musician.key().as_ref()], bump, space = 8 + 54 + 32 + 4 + 8 + 204 + 204 + 4)]
     pub music: Account<'info, Music>,
     #[account(mut)]
     pub signer: Signer<'info>,
+    #[account(mut)]
     pub musician: Account<'info, Musician>,
     pub system_program: Program<'info, System>,
 }
@@ -24,13 +25,14 @@ impl<'info> AddMusic<'info> {
         url_song: String,
     ) -> Result<()> {
         let music = &mut self.music;
+        let musician = &mut self.musician;
         music.name = name;
         music.artist = artist;
         music.style = style;
         music.price = price;
         music.url_cover = url_cover;
         music.url_song = url_song;
-        self.musician.music_amount += 1;
+        musician.music_amount += 1;
         Ok(())
     }
 }
